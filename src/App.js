7f19b813268177d5846b6c5df84f705f957d71fd
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-08-02 22:00:06 -0400

    established Form and Todo components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Things I Should Do</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
