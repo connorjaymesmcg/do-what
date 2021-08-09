5275ba21e78eb35a82c09f59c7abde55139ab315
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-08-09 13:57:37 -0400

    added local storage functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
   // Functions
@@ -28,6 +33,20 @@ function App() {
         break;
     }
   };
+
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  };
+
   return (
     <div className="App">
       <header>
