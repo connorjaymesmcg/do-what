cdd5328019a0cfc3925632332b41a2a59774d919
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-09-04 09:50:04 -0400

    Adjusted layout of mobile components

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -23,14 +23,14 @@ const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
         <button className="todo-button" type="submit" onClick={submitTodoHandler}>
           <i className="fas fa-plus-square"></i>
         </button>
-        <div className="select">
-          <select onChange={statusHandler} name="todos" className="filter-todo">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="uncompleted">Uncompleted</option>
-          </select>
-        </div>
       </form>
+      <div className="select">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
     </div>
   );
 };
